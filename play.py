3679e2babb2eb77dbc1b878ac4faa85744f398a2
sukhitashvili <sukhitashvili.soso@gmail.com>
2025-03-04 12:16:37 +0100

    reorganized and simlified the code, added more comments

--- play.py
+++ play.py
@@ -2,24 +2,29 @@ import argparse
 
 import ale_py
 import gymnasium as gym
+import numpy as np
 import torch
 
-from utils import display_observation, image_preprocess, policy_forward
+from utils import display_observation, image_preprocess
 
 
 @torch.no_grad()
 def main(args):
-    model = torch.load(args.model_path, weights_only=False)
-    model.eval()
-    model.to(args.device)
-
-    env = gym.make("Pong-v4", render_mode="rgb_array")  # render_mode="human" option fails on my PC, thus used opencv
-    observation = env.reset()
-    observation = observation[0]  # first observation is tuple of [numpy image, game info]
+    # init the parameters
     display = True
     D = 80 * 80  # input dimensionality: 80x80 grid
     prev_x = None
     reward_sum = 0
+
+    # load model
+    model = torch.load(args.model_path, weights_only=False)
+    model.eval()
+    model.to(args.device)
+
+    # init the game
+    env = gym.make("Pong-v4", render_mode="rgb_array")  # render_mode="human" option fails on my PC, thus used opencv
+    observation = env.reset()
+    observation = observation[0]  # first observation is tuple of [numpy image, game info]
 
     while True:
         # preprocess the observation, set input to network to be difference image
@@ -27,16 +32,18 @@ def main(args):
         input_x = cur_x - prev_x if prev_x is not None else torch.zeros(D).to(args.device)
         prev_x = cur_x
 
-        action = policy_forward(preprocessed_observation=input_x, model=model)  # Sample a random action
+        # model forward pass
+        output = model(input_x)
+        action = 2 if np.random.uniform() < output.item() else 3  # roll the dice!
 
         # step the environment and get new measurements
         observation, reward, terminated, truncated, info = env.step(action)
         reward_sum += reward
-
-        if display:  # hold any key pressed to watch the game
+        # display game if needed
+        if display:
             display_observation(observation=observation)
 
-        if terminated or truncated:  # an episode finished, 22 played games
+        if terminated or truncated:  # an episode finished, someone reached 22 scores
             print('Episode total reward:', reward_sum)
             break
 
